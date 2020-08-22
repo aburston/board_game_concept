d77e2739bc8b1c6a19980eb652df7113a12f580f
Ashley Burston <ashleyb@umbrae.co.uk>
2020-08-22 15:30:10 +0100

    added basic moving and attack resolution

--- game.py
+++ game.py
@@ -20,7 +20,6 @@ class UnitType:
     INITIAL = 0; 
     MOVING = 1;
     NOP = 2;
-    DESTROYED = 3;
 
     def __init__(self, name, symbol, speed, attack, health):
         self.name = name
@@ -43,6 +42,7 @@ class UnitType:
 
         self.state = UnitType.INITIAL
         self.direction = UnitType.NONE
+        self.destroyed = False
 
     def move(self, direction):
         self.state = UnitType.MOVING
@@ -51,12 +51,19 @@ class UnitType:
     def setName(self, name):
         self.name = name
 
-    def setBoard(self, board):
+    def setBoard(self, board, board_max_x, board_max_y):
         self.board = board
+        self.board_max_x = board_max_x
+        self.board_max_y = board_max_y
 
     def setCoords(self, x, y):
         self.x = x
         self.y = y
+
+    def incomingAttack(self, attack):
+        self.health = self.health - attack
+        if self.health <= 0:
+            destroyed = True
 
     # calculates attacks and marks units as DESTROYED, creates arrays of units in squares where multiple units are
     # trying to move simultaneously into the same square
@@ -65,11 +72,41 @@ class UnitType:
             # make sure that location on the board is empty
             assert self.board[self.x, self.y] is board.Empty, f"can't add {name} to board at ({x},{y})"
         elif self.state == UnitType.MOVING:
-            # process your move, including boundary conditions, attacks and defence calculations
-            # if there is a unit in the square, the attack process it triggered an calculated
-            # if there is no unit in the square the unit creates a list in the square and adds itself to the list
-            # if there is an array containing other units in the square it adds itself to the list
-            pass
+            if self.direction == UnitType.NORTH:
+                dest_y = self.y - 1
+                if self.y < 0:
+                    self.y = 0
+                    self.STATE = UnitType.NOP
+                    return
+            elif self.direction == UnitType.EAST:
+                dest_x = self.x + 1
+                if self.x > self.board_max_x:
+                    self.x = self.board_max_x
+                    self.STATE = UnitType.NOP
+                    return
+            elif self.direction == UnitType.SOUTH:
+                dest_y = self.y + 1
+                if self.y > self.board_max_y:
+                    self.y = self.board_max_y
+                    self.STATE = UnitType.NOP
+                    return
+            elif self.direction == UnitType.WEST:
+                dest_x = self.x - 1
+                if self.x < 0:
+                    self.x = 0
+                    self.STATE = UnitType.NOP
+                    return
+            else:
+                self.STATE = UnitType.NOP
+                return
+
+            if self.board[dest_x, dest_y] is board.Empty:
+                setCoords(dest_x, dest_y)
+                board[self.x, self.y] = [ self ]
+            elif self.board[dest_x, dest_y] is UnitType:
+                self.board[dest_x, dest_y].incomingAttack(self.attack)
+            self.STATE = UnitType.NOP
+            return
         else:
             pass
 
@@ -82,16 +119,24 @@ class UnitType:
             # add the unit to the board
             self.board[self.x, self.y] = self
         elif self.state == UnitType.MOVING:
-            # process your move, including boundary conditions, attacks and defence calculations
-            # if there is a unit in the square, the attack process it triggered an calculated
-            # if there is no unit in the square the unit creates a list in the square and adds itself to the list
-            # if there is an array containing other units in the square it adds itself to the list
-            pass
+            assert self.state == UnitType.MOVING, "During commit, no unit should be in the MOVING state" 
         else:
-            pass
-
-        # at the end of a commit set unit into the NOP state
-        self.state = UnitType.NOP
+            if type(self.board[self.x, self.y]) is list:
+                units = len(self.board[self.x, self.y])
+                while units >= 1:
+                    for unit in self.board[self.x, self.y]:
+                        for target in self.board[self.x, self.y]:
+                            if unit != target:
+                                target.incomingAttack(unit.attack)
+                    for unit in self.board[self.x, self.y]:
+                        if unit.destroyed:
+                            units = units - 1 
+                for unit in self.board[self.x, self.y]:
+                    if unit.destroyed == False:
+                        self.board[self.x, self.y] = unit
+            else:
+                if self.destroyed:
+                    self.board[self.x, self.y] = board.Empty
 
     def __str__(self):
         return(self.symbol)
@@ -110,19 +155,21 @@ class Board:
         assert ((size_y >= 2) and (size_y <= 10)), "size_y must be a value from 2 to 10"
 
         self.board = board.Board((size_x, size_y))
+        self.units = []
     
     def add(self, x, y, name, unit_type):
         # make a shallow copy of the unit type to create a new unit instance
         unit = copy.copy(unit_type)
         # reset the unit name
         unit.setName(name)
-        # add a ref to the board into the unit
-        unit.setBoard(self.board)
+        # add a ref to the board into the unit + the size
+        unit.setBoard(self.board, self.size_x, self.size_y)
         # keep a copy of the unit coords in the unit
         unit.setCoords(x,y)
         # add it to the board
         unit.preCommit()
         unit.commit()
+        self.units.append(unit)
 
     def print(self):
         self.board.draw()
