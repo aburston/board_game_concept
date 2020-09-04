bcfe0ddfdc41f653fb4c8b1601cb82435d5d7173
Ashley Burston <ashleyb@umbrae.co.uk>
2020-09-04 15:33:38 +0100

    added the set size command

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ DEBUG = True
 
 def usage():
    print("usage, client.py <gameno>", file = sys.stdin)
-   print("or, client.py <gameno> <playername>", file = sys.stderr)
+   print("or, client.py <gameno> <player_name>", file = sys.stderr)
 
 def command_help():
     print("""
@@ -40,29 +40,30 @@ exit - exit the game client
 
 def main(argv):
 
-    xsize = 2
-    ysize = 2
-    MAX_PLAYERS = 2
+    size_x = 0
+    size_y = 0
     game_password = ""
     password = ""
+    player_data = {}
+    board_meta_data = {}
 
     argc = len(argv)
     if DEBUG:
         print(f"len(argv): {argc}")
 
     if len(argv) == 2:
-        playername = '0'
+        player_name = '0'
         gameno = argv[1]
     elif len(argv) == 3:
         gameno = argv[1]
-        playername = argv[2]
+        player_name = argv[2]
     else:
         usage()
         sys.exit(1)
 
     basePath = os.getcwd() + "/games/_" + gameno
-    dataPath = basePath + "/data"
-    playerPath = basePath + "/players"
+    data_path = basePath + "/data"
+    player_path = basePath + "/players"
 
     if DEBUG:
         print(f"Basepath: {basePath}")
@@ -70,13 +71,15 @@ def main(argv):
     # try reading data file for game 
     # except to create the directory for the game data file
     try:
-        with open(dataPath + '/data.yaml') as f:
+        with open(data_path + '/data.yaml') as f:
             try:
-                data = yaml.safe_load(f)
+                board_meta_data = yaml.safe_load(f)
+                size_x = board_meta_data['board']['size_x']
+                size_y = board_meta_data['board']['size_y']
             except yaml.YAMLError as exc:
                 print(exc)
     except:
-        if playername == '0':
+        if player_name == '0':
             try:
                 print("Set game password")
                 password1 = getpass.getpass()
@@ -96,29 +99,28 @@ def main(argv):
             print(f"No game with number: {gameno}", file = sys.stderr)
             sys.exit(1)
     
-    if playername != '0':
+    if player_name != '0':
         password = getpass.getpass()
         try:
-            with open(playerPath + '/'+ playername + '.yaml') as f:
+            with open(player_path + '/'+ player_name + '.yaml') as f:
                 try:
-                    playerData = yaml.safe_load(f)
+                    player_data = yaml.safe_load(f)
                 except yaml.YAMLError as exc:
                     print(exc)
         except:
-            print(f"no player with name {playername}", file = sys.stderr)
+            print(f"no player with name {player_name}", file = sys.stderr)
             sys.exit(1)
  
-    #if player, retrieve data, check password
-    if playername != '0':
-        for filename in os.listdir(playerPath):
+    # if player, retrieve data, check password
+    if player_name != '0':
+        for filename in os.listdir(player_path):
             if filename.endswith(".yaml"):
                 filename = os.path.splitext(filename)[0] 
-                if filename==playername:
-                    assert playerData['password']==password, "password doesn't match username"
-                    xsize = data['board']['size_x']
-                    ysize = data['board']['size_y']
+                if filename == player_name:
+                    assert player_data['password']==password, "password doesn't match username"
 
     # candidate config, this should be populated from any existing files
+
     players = {}
     name = []
     symbol = []
@@ -148,11 +150,29 @@ def main(argv):
             command_help()
             continue
 
+        # set - size, player
+        if tokens[0]=='set':
+            if DEBUG:
+                print(f"len(tokens): {len(tokens)}")
+            if tokens[1] == 'size':
+                if len(tokens) != 4:
+                    print("must provide x and y for size")
+                    continue
+                try:
+                    size_x = int(tokens[2])
+                    size_y = int(tokens[3])
+                except:    
+                    print("x and y must be a numbers")
+                    continue
+                if size_x < 2:
+                    print("x must be greater than 1")
+                if size_y < 2:
+                    print("y must be greater than 1")
+
         # add - player, type, unit
         if tokens[0]=='add':
-            assert len(tokens)>=2,"invalid add command"
             if tokens[1] == 'player':
-                if playername != '0':
+                if player_name != '0':
                     print("only the game admin (player '0') can add players")
                     continue
                 if len(tokens) != 4:
@@ -188,34 +208,37 @@ def main(argv):
                 print("invalid add command")
                 continue
 
-# =============================================================================
-#         if token[0]=='commit':
-#             commit()
-#         if token[0]=='move':
-#             assert len(tokens)==5,"must provide 3 args for player"
-# =============================================================================
-
         if tokens[0] == 'exit':
             break
 
-        #initialising the game savees all input data to yaml for the game setup step
+        # commiting the game saves all input data to yaml for the game setup step
         if tokens[0] == 'commit':
 
-            # check there are enough players
-            if len(players.keys())<MAX_PLAYERS:
-                print ("not enough players")
+            # check the board size has been set
+            if size_x <= 1 or size_y <= 1:
+                print(f"the board size is too small ({size_x}, {size_y})")
                 continue
 
             # add required directories
-            if not(os.path.exists(dataPath)):
-                os.makedirs(dataPath)
-            if not(os.path.exists(playerPath)):
-                os.makedirs(playerPath)
+            if not(os.path.exists(data_path)):
+                os.makedirs(data_path)
+            if not(os.path.exists(player_path)):
+                os.makedirs(player_path)
 
             # write the data file for the board
-            dict_file = {'board' : {'size_x' : xsize,'size_y' : ysize},'game' : {'game' : gameno,'no_of_players' : MAX_PLAYERS}}
-            with open(dataPath + '/data.yaml', 'w') as file:
-                yaml.safe_dump(dict_file, file)
+            board_meta_data = {
+                'board' : {
+                    'size_x' : size_x,
+                    'size_y' : size_y
+                },
+                'game' : {
+                    'game' : gameno,
+                    'no_of_players' : len(players.keys()),
+                    'password': game_password
+                },
+            }
+            with open(data_path + '/data.yaml', 'w') as file:
+                yaml.safe_dump(board_meta_data, file)
 
             # write the individual player files
             for p in players.keys():
@@ -224,10 +247,10 @@ def main(argv):
                     'email': players[p]['email'],
                     'password': players[p]['password']
                 }
-                with open(playerPath + '/'+ p + '.yaml', 'w') as file:
+                with open(player_path + '/'+ p + '.yaml', 'w') as file:
                     yaml.safe_dump(player_dict, file)
 
-        #use to update player yaml to include added unit types    
+        # use to update player yaml to include added unit types    
         if tokens[0] == 'commit_types':
             unitData = {
                 'unit_types' : {
@@ -240,10 +263,10 @@ def main(argv):
                 }        
             }
 
-        #use to update player yaml to unclude placed units
+        # use to update player yaml to unclude placed units
         if tokens[0] == 'place_units':
             for i in utype:
-                if i in playerData['unit_types']:
+                if i in player_data['unit_types']:
                     continue
                 else:
                     print("no unit of type %s" % i)
